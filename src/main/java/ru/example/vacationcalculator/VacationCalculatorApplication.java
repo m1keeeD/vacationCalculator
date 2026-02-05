8ed42e4d634ad7bd29ab0448074c36e429aafadd
m1keeeD <l1nerunn3r@gmail.com>
2026-02-05 23:04:02 +0400

    Created project structure

---
+++ src/main/java/ru/example/vacationcalculator/VacationCalculatorApplication.java
@@ -0,0 +1,5 @@
+package main.java.ru.example.vacationcalculator;
+
+public class VacationCalculatorApplication {
+    
+}
---
+++ src/main/java/ru/example/vacationcalculator/model/VacationResponse.java
@@ -0,0 +1,5 @@
+package main.java.ru.example.vacationcalculator.model;
+
+public class VacationResponse {
+    
+}
