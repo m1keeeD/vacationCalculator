1571b19092a60c59e061929b55107f094de2d7ba
m1keeeD <m1keeeD@proton.me>
2026-02-08 14:56:56 +0330

    Added minimal code to VacationController: made my app write a simple message in localhost. Declared endpoint

--- src/main/java/ru/example/vacationcalculator/controller/VacationController.java
+++ src/main/java/ru/example/vacationcalculator/controller/VacationController.java
@@ -1,5 +1,15 @@
 package ru.example.vacationcalculator.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class VacationController {
     
+    @GetMapping("/calculate")
+    public String calculate() {
+        return "Vacation calculator is working";
+    }   
+
 }
